edfe00d77972313345daf309a6d81b1bb8be0951
braydend <braydendiluca@gmail.com>
2017-10-02 11:41:34 +1100

    Fixed client closing after first file
    Client can now download multiple files, one after another.
    Fixed indentaion in files.
    Files now download to dynamically created directory "Server Downloaded
    Files"

--- Client/SimpleFileClient.java
+++ Client/SimpleFileClient.java
@@ -11,88 +11,97 @@ import java.net.Socket;
 
 public class SimpleFileClient {
 
-  public final static int SOCKET_PORT = 13267;      // you may change this
-  public final static String SERVER = "192.168.0.33";  // localhost
+	public final static int SOCKET_PORT = 13267;      // you may change this
+	public final static String SERVER = "192.168.0.33";  // bryaden's local IP
  
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//For reading user input from terminal
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//For reading user input from terminal
 
-  public final static int FILE_SIZE = 5500000; // file size hard coded 5.5MB
+	public final static int FILE_SIZE = 5500000; // file size hard coded 5.5MB
 
-  public static void main (String [] args ) throws IOException {
-	  File FILE_TO_RECEIVED = null;  // set name when file is selected
-    int bytesRead;
-    int current = 0;
-    FileOutputStream fos = null;
-    BufferedOutputStream bos = null;
-    Socket sock = null;
-    try {
-      sock = new Socket(SERVER, SOCKET_PORT);
-      System.out.println("Connecting...");
-      
-      //Send message to server choosing which file to send
-      int selectFile = 0;
-      OutputStream outStream = sock.getOutputStream();
-      DataOutputStream dos = new DataOutputStream(outStream);
-      System.out.println("Select a file to download from server: ");
-      System.out.println("1) Image File");
-      System.out.println("2) Music File");
-      System.out.println("3) CSV File");
-      System.out.println("4) PDF File");
-      System.out.println("5) Video File");
-      //Read choice
-      try{
-	      selectFile = Integer.parseInt(br.readLine());
-	      //Validate input
-	      while(selectFile < 1 || selectFile > 5){
-	    	  System.out.println("Please enter a value (1-5)");
-	    	  selectFile = Integer.parseInt(br.readLine());
-	    	  
-	      }
-	      //Set output file based on selection
-	      switch(selectFile){
-	      case 1: FILE_TO_RECEIVED = new File("download.jpg");
-	      break;
-	      case 2: FILE_TO_RECEIVED = new File("download.mp3");
-	      break;
-	      case 3: FILE_TO_RECEIVED = new File("download.csv");
-	      break;
-	      case 4: FILE_TO_RECEIVED = new File("download.pdf");
-	      break;
-	      case 5: FILE_TO_RECEIVED = new File("download.mp4");
-	      break;
-	      }
-	    //Send choice to server
-	      dos.writeInt(selectFile);
-      }catch (Exception e){
-    	  System.out.println("ERROR: Incorrect input");
-      }
-      System.out.println("LOG: FILE SELECTED: " + selectFile);
-      
-      //Receive file from server
-      byte [] mybytearray  = new byte [FILE_SIZE];
-      InputStream is = sock.getInputStream();
-      fos = new FileOutputStream(FILE_TO_RECEIVED);
-      bos = new BufferedOutputStream(fos);
-      bytesRead = is.read(mybytearray,0,mybytearray.length);
-      System.out.println("Bytes Read: " + bytesRead);
-      current = bytesRead;
-
-      do {
-         bytesRead =
-            is.read(mybytearray, current, (mybytearray.length-current));
-         if(bytesRead >= 0) current += bytesRead;
-      } while(bytesRead > -1);
-
-      bos.write(mybytearray, 0 , current);
-      bos.flush();
-      System.out.println("File " + FILE_TO_RECEIVED
-          + " downloaded (" + current + " bytes read)");
-    }
-    finally {
-      if (fos != null) fos.close();
-      if (bos != null) bos.close();
-      if (sock != null) sock.close();
-    }
-  }
+	public static void main (String [] args ) throws IOException {
+		File FILE_TO_RECEIVED = null;  // set name when file is selected
+		int bytesRead;
+		int current = 0;
+		FileOutputStream fos = null;
+		BufferedOutputStream bos = null;
+		Socket sock = null;
+		try {
+			while(true){
+				//Connect to server
+				sock = new Socket(SERVER, SOCKET_PORT);
+				System.out.println("Connecting...");
+				//Send message to server choosing which file to send
+				int selectFile = 0;
+				OutputStream outStream = sock.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(outStream);
+				//Print menu to console
+				printMenu();
+				//Read choice
+				try{
+					selectFile = Integer.parseInt(br.readLine());
+					//Validate input
+					while(selectFile < 1 || selectFile > 5){
+						System.out.println("Please enter a value (1-5)");
+						selectFile = Integer.parseInt(br.readLine());  
+					}
+					//Set output file based on selection
+					switch(selectFile){
+					    case 1: FILE_TO_RECEIVED = new File("Server Downloaded Files/download.jpg");
+					    break;
+					    case 2: FILE_TO_RECEIVED = new File("Server Downloaded Files/download.mp3");
+					    break;
+					    case 3: FILE_TO_RECEIVED = new File("Server Downloaded Files/download.csv");
+					    break;
+					    case 4: FILE_TO_RECEIVED = new File("Server Downloaded Files/download.pdf");
+					    break;
+					    case 5: FILE_TO_RECEIVED = new File("Server Downloaded Files/download.mp4");
+					    break;
+					}
+					//Send choice to server
+					dos.writeInt(selectFile);
+				}catch (Exception e){
+					System.out.println("ERROR: Incorrect input");
+				}
+				System.out.println("LOG: FILE SELECTED: " + selectFile);
+			    //Create directory for files
+			    boolean successful =  (new File("Server Downloaded Files")).mkdir();
+			    if (successful){	System.out.println("directory was created successfully");	}	//Directory created successfully
+			    else{	System.out.println("failed trying to create the directory");	}	//Directory failed to create
+			     
+			    //Receive file from server
+			    byte [] mybytearray  = new byte [FILE_SIZE];
+			    InputStream is = sock.getInputStream();
+			    fos = new FileOutputStream(FILE_TO_RECEIVED);
+			    bos = new BufferedOutputStream(fos);
+			    bytesRead = is.read(mybytearray,0,mybytearray.length);
+			    System.out.println("Bytes Read: " + bytesRead);
+			    current = bytesRead;
+	
+			    do {
+			    	bytesRead = is.read(mybytearray, current, (mybytearray.length-current));
+			    	if(bytesRead >= 0) current += bytesRead;
+			    } while(bytesRead > -1);
+	
+			    bos.write(mybytearray, 0 , current);
+			    bos.flush();
+			    System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
+			}
+		}
+		finally {
+			if (fos != null) fos.close();
+			if (bos != null) bos.close();
+			if (sock != null) sock.close();
+		}
+	}
+  
+	//Print menu to console
+	public void printMenu(){
+		System.out.println("Select a file to download from server: ");
+		System.out.println("1) Image File");
+		System.out.println("2) Music File");
+		System.out.println("3) CSV File");
+		System.out.println("4) PDF File");
+		System.out.println("5) Video File");
+	}
 
 }
--- Server/SimpleFileServer.java
+++ Server/SimpleFileServer.java
@@ -10,76 +10,76 @@ import java.net.Socket;
 
 public class SimpleFileServer {
 
-  public final static int SOCKET_PORT = 13267;  // you may change this
-  public final static String IMG_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/leroy.jpg";  //Image file to send
-  public final static String MP3_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/RickRoll.mp3";  //MP3 file to send
-  public final static String CSV_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SampleCSV.csv";  //CSV file to send
-  public final static String PDF_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SamplePDF.pdf";  //PDF file to send
-  public final static String MP4_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SampleVid.mp4";  //MP4 file to send
+	public final static int SOCKET_PORT = 13267;  // you may change this
+	public final static String IMG_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/leroy.jpg";  //Image file to send
+	public final static String MP3_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/RickRoll.mp3";  //MP3 file to send
+	public final static String CSV_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SampleCSV.csv";  //CSV file to send
+	public final static String PDF_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SamplePDF.pdf";  //PDF file to send
+	public final static String MP4_FILE = "C:/Users/BJ/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SampleVid.mp4";  //MP4 file to send
   
 
-  public static void main (String[] args) throws IOException {
-    FileInputStream fis = null;
-    BufferedInputStream bis = null;
-    OutputStream os = null;
-    ServerSocket servsock = null;
-    Socket sock = null;
-    File fileSending = null;
-    int fileRequested = 0;
+	public static void main (String[] args) throws IOException {
+		FileInputStream fis = null;
+		BufferedInputStream bis = null;
+		OutputStream os = null;
+		ServerSocket servsock = null;
+		Socket sock = null;
+		File fileSending = null;
+		int fileRequested = 0;
     
-    try {
-      servsock = new ServerSocket(SOCKET_PORT);
-      while (true) {
-        System.out.println("Waiting...");
-        try {
-          sock = servsock.accept();
-          System.out.println("Accepted connection : " + sock);
-          while(fileRequested == 0){
-        	  //Input stream
-        	  InputStream is = sock.getInputStream();
-        	  System.out.println("Input stream open");
-        	  //Data input stream
-        	  DataInputStream dis = new DataInputStream(is);
-        	  System.out.println("Data stream open");
-        	  System.out.println("Waiting for selection....");
-        	  fileRequested = dis.readInt();
-        	  System.out.println("File Requested : " + fileRequested);
-          }
-          // send file
-          switch(fileRequested){
-          	case 1: fileSending = new File(IMG_FILE);
-          	break;
-          	case 2: fileSending = new File(MP3_FILE);
-          	break;
-          	case 3: fileSending = new File(CSV_FILE);
-          	break;
-          	case 4: fileSending = new File(PDF_FILE);
-          	break;
-          	case 5: fileSending = new File(MP4_FILE);
-          	break;
-          }
-          byte [] mybytearray  = new byte [(int)fileSending.length()];
-          fis = new FileInputStream(fileSending);
-          bis = new BufferedInputStream(fis);
-          bis.read(mybytearray,0,mybytearray.length);
-          os = sock.getOutputStream();
-          //Print info on file being sent
-          System.out.println("Sending " + fileSending.getName() + "(" + mybytearray.length + " bytes)");
-          os.write(mybytearray,0,mybytearray.length);
-          os.flush();
-          System.out.println("Done.");
-          //reset selection
-          fileRequested = 0;
-        }
-        finally {
-          if (bis != null) bis.close();
-          if (os != null) os.close();
-          if (sock!=null) sock.close();
-        }
-      }
-    }
-    finally {
-      if (servsock != null) servsock.close();
-    }
-  }
+		try {
+			servsock = new ServerSocket(SOCKET_PORT);
+			while (true) {
+				System.out.println("Waiting...");
+				try {
+					sock = servsock.accept();
+					System.out.println("Accepted connection : " + sock);
+					while(fileRequested == 0){
+						//Input stream
+						InputStream is = sock.getInputStream();
+						System.out.println("Input stream open");
+						//Data input stream
+						DataInputStream dis = new DataInputStream(is);
+						System.out.println("Data stream open");
+						System.out.println("Waiting for selection....");
+						fileRequested = dis.readInt();
+						System.out.println("File Requested : " + fileRequested);
+					}
+					//send file
+					switch(fileRequested){
+						case 1: fileSending = new File(IMG_FILE);
+						break;
+						case 2: fileSending = new File(MP3_FILE);
+						break;
+						case 3: fileSending = new File(CSV_FILE);
+						break;
+						case 4: fileSending = new File(PDF_FILE);
+						break;
+						case 5: fileSending = new File(MP4_FILE);
+						break;
+					}
+					byte [] mybytearray  = new byte [(int)fileSending.length()];
+					fis = new FileInputStream(fileSending);
+					bis = new BufferedInputStream(fis);
+					bis.read(mybytearray,0,mybytearray.length);
+					os = sock.getOutputStream();
+					//Print info on file being sent
+					System.out.println("Sending " + fileSending.getName() + "(" + mybytearray.length + " bytes)");
+					os.write(mybytearray,0,mybytearray.length);
+					os.flush();
+					System.out.println("Done.");
+					//reset selection
+					fileRequested = 0;
+				}
+				finally {
+					if (bis != null) bis.close();
+					if (os != null) os.close();
+					if (sock!=null) sock.close();
+				}
+			}
+		}
+		finally {
+			if (servsock != null) servsock.close();
+		}
+	}
 }
