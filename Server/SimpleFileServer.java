2abf6e4e5ded1fc41b0d4920ac2a9c6c4d4910fd
braydend <braydendiluca@gmail.com>
2017-10-09 12:17:32 +1100

    Cleaned up comments

--- Server/SimpleFileServer.java
+++ Server/SimpleFileServer.java
@@ -10,7 +10,8 @@ import java.net.Socket;
 
 public class SimpleFileServer {
 
-	public final static int SOCKET_PORT = 13267;  // you may change this
+	public final static int SOCKET_PORT = 13267;  //Port that will be used for the socket
+	//Locations for files on server. Change based on absolute locations on server
 	public final static String IMG_FILE = "C:/Users/brayd/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/leroy.jpg";  //Image file to send
 	public final static String MP3_FILE = "C:/Users/brayd/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/RickRoll.mp3";  //MP3 file to send
 	public final static String CSV_FILE = "C:/Users/brayd/Google Drive/Semester 2 2017/Data Communications and Security/Assignment 2/TCP_File_Server/files/SampleCSV.csv";  //CSV file to send
@@ -19,6 +20,7 @@ public class SimpleFileServer {
   
 
 	public static void main (String[] args) throws IOException {
+		//Vars and objects for transfer
 		FileInputStream fis = null;
 		BufferedInputStream bis = null;
 		OutputStream os = null;
@@ -28,20 +30,24 @@ public class SimpleFileServer {
 		int fileRequested = 0;
     
 		try {
+			//Create server socket
 			servsock = new ServerSocket(SOCKET_PORT);
 			System.out.println("Server Started...");
 			while (true) {
 				try {
+					//Accept connection on socket
 					sock = servsock.accept();
 					System.out.println("Accepted connection to client : " + sock);
+					//While no selection from client
 					while(fileRequested == 0){
 						//Input stream
 						InputStream is = sock.getInputStream();
-						//System.out.println("LOG: Input stream open");
+						//System.out.println("TESTING: Input stream open");
 						//Data input stream
 						DataInputStream dis = new DataInputStream(is);
-						//System.out.println("LOG: Data stream open");
+						//System.out.println("TESTING: Data stream open");
 						System.out.println("Waiting for selection....");
+						//Read selection from client
 						fileRequested = dis.readInt();
 					}
 					//send file
@@ -62,23 +68,30 @@ public class SimpleFileServer {
 						System.out.println("File Requested : SampleVid.mp4");
 						break;
 					}
+					//Create byte arrays with the size of the file selected
 					byte [] mybytearray  = new byte [(int)fileSending.length()];
 					byte [] encbytearray  = new byte [(int)fileSending.length()];
+					//Create streams to read file
 					fis = new FileInputStream(fileSending);
 					bis = new BufferedInputStream(fis);
+					//Read file
 					bis.read(mybytearray,0,mybytearray.length);
+					//get output stream to send messages to client
 					os = sock.getOutputStream();
 					//Encrypt bytes to be sent
 					encbytearray = encryptByteArray(mybytearray);
 					//Print info on file being sent
 					System.out.println("Sending " + fileSending.getName() + "(" + encbytearray.length + " bytes)");
+					//Send file to client
 					os.write(encbytearray,0,encbytearray.length);
+					//Clear buffer in output stream
 					os.flush();
 					System.out.println("File transfer complete");
 					//reset selection
 					fileRequested = 0;
 				}
 				finally {
+					//close streams and socket
 					if (bis != null) bis.close();
 					if (os != null) os.close();
 					if (sock!=null) sock.close();
@@ -86,26 +99,30 @@ public class SimpleFileServer {
 			}
 		}
 		finally {
+			//Close server socket
 			if (servsock != null) servsock.close();
 		}
 	}
 	
 	public static byte[] encryptByteArray(byte[] in){
+		//Byte array created to hold file being encrypted
 		byte[] encryptedArray = in;
+		//Shared key for cypto
 		StringBuffer key = new StringBuffer("KazuyaDanielBrayden");
+		//Var to hold integer value fo shared key
 		int sharedKey = 0;
 		//Print status to console
 		System.out.println("Encrypting file...");
 		//Calculate shared key
 		for(int i = 0; i < key.length(); i++){
 			sharedKey += (int)key.charAt(i);
-			//System.out.println("LOG: sharedkey = " + sharedKey);
+			//System.out.println("TESTING: sharedkey = " + sharedKey);
 		}
 		//Encrypt bits
 		for(int j = 0; j < in.length; j++){
-			//System.out.println("LOG: original byte = " + in[j]);
+			//System.out.println("TESTING: original byte = " + in[j]);
 			encryptedArray[j] = (byte) ((int) in[j] + sharedKey);
-			//System.out.println("LOG: new byte = " + encryptedArray[j]);
+			//System.out.println("TESTING: new byte = " + encryptedArray[j]);
 		}
 		//Print status to console
 		System.out.println("File Encrypted");
